9dae2ddde8d38b904fa8053b6cd0a49a252e4bcb
bob4321at <judeberin113@gmail.com>
2026-07-11 18:02:16 -0600

    random song feature

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
 	"slices"
@@ -35,7 +36,7 @@ type Root struct {
 var Selected_Song int
 
 func DownloadSong(SongUrl, path string) {
-	download := exec.Command("yt-dlp", "--extractor-args", "youtube:player_client=web_safari", "-o", path, "-x", "--audio-format", "mp3", SongUrl)
+	download := exec.Command("yt-dlp", "-o", path, "-x", "--audio-format", "mp3", SongUrl)
 
 	if err := download.Run(); err != nil {
 		log.Fatal(err)
@@ -83,14 +84,93 @@ func (r *Root) Tick(context *guigui.Context, widgetBounds *guigui.WidgetBounds) 
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyControl) && inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		if Ran {
+		if !Ran {
 			clipboardText := string(clipboard.Read(clipboard.FmtText))
 
-			if strings.Contains(clipboardText, "youtube.com") {
-				go DownloadSong(clipboardText, r.directory_ui.BaseDirectory+"/"+r.directory_select.Value())
-			}
+			go DownloadSong(clipboardText, r.directory_ui.BaseDirectory+"/"+r.directory_select.Value())
 		}
 		Ran = true
+	}
+
+	if !ebiten.IsKeyPressed(ebiten.KeyD) && ebiten.IsKeyPressed(ebiten.KeyControl) {
+		Ran = false
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyR) && ebiten.IsKeyPressed(ebiten.KeyControl) {
+		total_songs := []string{}
+
+		dirs_to_check := []string{}
+
+		start_dir := r.directory_ui.BaseDirectory
+
+		first_dir, err := os.ReadDir(start_dir)
+		if err != nil {
+			panic(err)
+		}
+
+		for i := range first_dir {
+			item := first_dir[i]
+			if item.IsDir() {
+				new_dir_to_check := r.directory_ui.BaseDirectory + "/" + item.Name()
+				dirs_to_check = append(dirs_to_check, new_dir_to_check)
+			} else {
+				new_song_to_check := r.directory_ui.BaseDirectory + "/" + item.Name()
+				total_songs = append(total_songs, new_song_to_check)
+			}
+		}
+
+		new_base_dir := ""
+
+		for len(dirs_to_check) > 0 {
+			for i := range dirs_to_check {
+				if i >= len(dirs_to_check) {
+					break
+				}
+				next_dir_path := dirs_to_check[i]
+				next_dir, err := os.ReadDir(next_dir_path)
+				if err != nil {
+					panic(err)
+				}
+
+				new_base_dir = dirs_to_check[i]
+
+				RemoveArrayElement(i, &dirs_to_check)
+
+				for j := range next_dir {
+					item := next_dir[j]
+
+					if item.IsDir() {
+						new_dir_to_check := new_base_dir + "/" + item.Name()
+						dirs_to_check = append(dirs_to_check, new_dir_to_check)
+					} else {
+						new_song_to_check := new_base_dir + "/" + item.Name()
+						total_songs = append(total_songs, new_song_to_check)
+					}
+				}
+			}
+		}
+
+		random_num := rand.Intn(len(total_songs))
+		for random_num >= len(total_songs) {
+			random_num = rand.Intn(len(total_songs))
+		}
+		chosen_song := total_songs[random_num]
+
+		_, test2, _ := strings.Cut(chosen_song, homepath+"/Music/")
+		test2 += "^"
+
+		err = os.Truncate(homepath+"/Documents/current_song", 0)
+		if err != nil {
+			panic(err)
+		}
+		f, err := os.OpenFile(homepath+"/Documents/current_song", os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		f.WriteString(test2)
+		f.Close()
+
+		os.Exit(0)
 	}
 
 	if !ebiten.IsKeyPressed(ebiten.KeyShift) && inpututil.IsKeyJustPressed(ebiten.KeyTab) {
